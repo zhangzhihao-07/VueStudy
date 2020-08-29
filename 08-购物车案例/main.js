7e598c1e591942ebead5a2499f38f673a7261079
zhangzhihao-07 <1121102035@qq.com>
2020-08-29 10:54:12 +0800

    8/29

---
+++ 08-购物车案例/main.js
@@ -0,0 +1,63 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    list:[
+      {
+        id: 1,
+        name: '第1本书',
+        price: 100,
+        count: 1
+      },
+      {
+        id: 2,
+        name: '第2本书',
+        price: 120,
+        count: 1
+      },
+      {
+        id: 3,
+        name: '第3本书',
+        price: 130,
+        count: 1
+      },
+      {
+        id: 4,
+        name: '第4本书',
+        price: 140,
+        count: 1
+      },
+    ],
+
+  },
+  methods: {
+    // getPrice(price) {
+    //   return '￥' + price.toFixed(2);
+    // }
+    add(index) {
+      // console.log(index);
+      this.list[index].count++;
+    },
+    unadd(index) {
+      // console.log(index);
+        this.list[index].count--;
+    },
+    reMove(index) {
+      this.list.splice(index,1)
+    }
+  },
+  computed: {
+    sumPrice() {
+      let sum = 0;
+      for(let i=0; i<this.list.length; i++){
+        sum += this.list[i].price * this.list[i].count;
+      }
+      return sum;
+    }
+    
+  },
+  filters: {
+    showPrice(price) {
+      return '￥' + price.toFixed(2);
+    }
+  }
+})
